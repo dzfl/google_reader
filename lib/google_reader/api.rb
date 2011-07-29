6a07900440f6bf7bba44f8d39274cd104d42a3b7
AOKI Hanae <aereal@kerare.org>
2011-07-30 05:44:00 +0900

    refactoring

--- lib/google_reader/api.rb
+++ lib/google_reader/api.rb
@@ -1,38 +1,47 @@
+# encoding: utf-8
 
-module GoogleReader
-
-	class Api
-		attr_reader :auth, :token
-		def initialize(source, service)
-			@source  = source  || "application-identifier"
-			@service = service || "service-identifier"
-			@auth
-			@token
-		end
-
-		def login(email, password)
-			login = Login.new(email, password, @source, @service)
-			login.authenticate
-			@auth = login.auth
-			@token = get_token
-		end
-
-		def get_token
-			resbody = get("/reader/api/0/token")
-			resbody.sub(/\/\//, "")
-		end
-
-		#private
-		def get(path)
-			req = Net::HTTP::Get.new(path)
-			req.add_field "Authorization", "GoogleLogin auth=#{@auth}"
-			Net::HTTP.new('www.google.com', 80).start do |http|
-				res = http.request(req)
-				res.body
-			end
-		end
-
+class GoogleReader::API
+	def initialize(source, service)
+		@source  = source  || "application-identifier"
+		@service = service || "reader"
+		@end_point = URI("http://www.google.com/reader")
 	end
 
+	def login(email, password)
+		login = Login.new(email, password, @source, @service)
+		login.authenticate
+		@auth = login.auth
+		@token = get_token
+	end
+
+	def get_token
+		resbody = get("/api/0/token")
+		resbody.sub(/\/\//, "")
+	end
+
+	def authorization_headers
+		{"Authorization" => "GoogleLogin auth=#{@auth}"}
+	end
+
+	#private
+	def get(path, params={})
+		uri = @end_point + path
+		uri.query = URI.encode_www_form(params)
+		req = Net::HTTP::Get.new(uri.request_uri, authorization_headers)
+		Net::HTTP.start(uri.host, uri.port) do |http|
+			res = http.request(req)
+			res.body
+		end
+	end
+
+	def post(path, params={})
+		uri = @end_point + path
+		data = URI.encode_www_form(params)
+		req = Net::HTTP::Post.new(uri.request_uri, authorization_headers)
+		Net::HTTP.start("www.google.com", 80) do |http|
+			res = http.request(req, data)
+			res.body
+		end
+	end
 end
 
--- lib/google_reader/login.rb
+++ lib/google_reader/login.rb
@@ -1,64 +1,58 @@
-module GoogleReader
+# encoding: utf-8
+require "net/https"
+require "uri"
 
-	class Login
-		require 'net/https'
-		require 'uri'
-		
-		LoginError = Class.new Exception
+class GoogleReader::Login
+	class LoginError < GoogleReaderError; end
 
-		attr_reader :auth, :sid, :lsid
+	attr_reader :auth, :sid, :lsid
 
-		def initialize(email, password, source=nil, service=nil)
-			@email = email
-			@password = password
-			@service = service || "service-identifier"
-			@source  = source  || "application-identifier"
-			@auth
-			@sid
-			@lsid
-		end
+	def initialize(email, password, source=nil, service=nil)
+		@email = email
+		@password = password
+		@service = service || "reader"
+		@source  = source  || "application-identifier"
+		@auth
+		@sid
+		@lsid
+	end
 
-		def loggedin?
-			@auth ? true : false
-		end
+	def loggedin?
+		@auth ? true : false
+	end
 
-		def authenticate
-			auth_hash = Hash.new
+	def authenticate
+		auth_hash = Hash.new
 
-			url = URI.parse("https://www.google.com/accounts/ClientLogin")
-			https = Net::HTTP.new(url.host, url.port)
+		url = URI.parse("https://www.google.com/accounts/ClientLogin")
+		req = Net::HTTP::Post.new(url.request_uri)
+		req.set_form_data(
+			:accountType => 'GOOGLE',
+			:Email       => @email,
+			:Passwd      => @password,
+			:service     => @service,
+			:source      => @source
+		)
+		Net::HTTP.start(url.host, url.port) do |https|
 			https.use_ssl = true
 			https.verify_mode = OpenSSL::SSL::VERIFY_PEER
 			https.ca_file = "/etc/ssl/certs/ca-certificates.crt"
 			https.verify_depth = 5
-			req = Net::HTTP::Post.new(url.path)
-			req.set_form_data({
-				:accountType => 'GOOGLE',
-				:Email       => @email,
-				:Passwd      => @password,
-				:service     => @service,
-				:source      => @source
-			})
-			res = ""
-			https.start do
-				res = https.request(req)
-				if res.code == "200"
-					res.body.split("\n").each do |data|
-						key,value = data.split("=")
-						auth_hash[key.downcase] = value
-					end
+			res = https.request(req)
+			case res
+			when Net::HTTPSuccess
+				res.body.split("\n").each do |data|
+					key,value = data.split("=")
+					auth_hash[key.downcase] = value
 				end
 			end
-
-			@sid  = auth_hash['sid']
-			@lsid = auth_hash['lsid']
-			@auth = auth_hash['auth']
-
-		rescue e
-			false
 		end
 
+		@sid  = auth_hash['sid']
+		@lsid = auth_hash['lsid']
+		@auth = auth_hash['auth']
+	rescue e
+		false
 	end
-
 end
 
